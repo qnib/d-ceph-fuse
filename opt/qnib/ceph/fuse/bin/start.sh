067c290158a833b9812088e86e63dbf3112371be
Christian Kniep <christian@qnib.org>
2015-12-14 15:05:49 +0100

    move consul-templates here

--- opt/qnib/ceph/fuse/bin/start.sh
+++ opt/qnib/ceph/fuse/bin/start.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-sleep 2
 source /opt/qnib/consul/etc/bash_functions.sh
+wait_for_srv ceph-mds
 
-wait_for_srv ceph-mon
+curl -s "localhost:8500/v1/kv/ceph/?keys" |grep ceph.client.admin.keyring >/dev/null
+EC=$?
+while [ ${EC} -ne 0 ];do
+    echo "sleep 1"
+	sleep 1
+    curl -s "localhost:8500/v1/kv/ceph/?keys" |grep ceph.client.admin.keyring >/dev/null
+    EC=$?
+done
 
 # Create keyring
-consul-template -once -template /etc/consul-templates/templates/ceph.client.admin.keyring.ctmpl:/etc/ceph/ceph.client.admin.keyring
+consul-template -once -template /etc/consul-templates/ceph/ceph.client.admin.keyring.ctmpl:/etc/ceph/ceph.client.admin.keyring
 # create conf
-consul-template -once -template /etc/consul-templates/templates/ceph.conf.ctmpl:/etc/ceph/ceph.conf
+consul-template -once -template /etc/consul-templates/ceph/ceph.conf.ctmpl:/etc/ceph/ceph.conf
 
 # create /ceph
-mkdir /ceph
+mkdir -p /ceph
 # start fuse
-ceph-fuse /ceph
+ceph-fuse -f /ceph
 
